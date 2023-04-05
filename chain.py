fa1fcaac556dfc25a1484abda1ef7524f1d14638
Kaiser Pister <pisterk@gmail.com>
2023-04-05 14:26:55 -0500

    add chroma cache

--- chain.py
+++ chain.py
@@ -1,4 +1,5 @@
-from langchain.embeddings.openai import OpenAIEmbeddings
+import glob
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.chains import VectorDBQAWithSourcesChain
@@ -8,22 +9,37 @@ from langchain.prompts.chat import (
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate,
 )
+import os
 
+def basic_parser(filename, model):
+    """
+    Takes in a .txt file and returns a list of strings, each string being a sentence.
+    """
+    path = os.path.join(os.getcwd(), "transcriptions", filename)
+    with open(path, 'r') as f:
+        content = f.read()
+        sentences = content.split('\n')
+    
+    embeddings = model.encode(sentences)
+    return sentences, embeddings
 
-def load_vectorstore(transcription_folder: str):
-    # TODO use transcription folder
-    with open("../../state_of_the_union.txt") as f:
-        state_of_the_union = f.read()
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    texts = text_splitter.split_text(state_of_the_union)
+def create_vectorstore(transcription_folder: str):
+    files = glob.glob(transcription_folder + "/*.txt")
+    texts = []
+    text_splitter = CharacterTextSplitter(separator="\n", chunk_size=512, chunk_overlap=64)
+    for file in files:
+        with open(file) as f:
+            text = f.read()
+            texts.extend(text_splitter.split_text(text))
 
-    embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceEmbeddings()
 
-    # TODO cache this, only load on new transcription files
     return Chroma.from_texts(
-        texts, embeddings, metadatas=[{"source": f"{i}-pl"} for i in range(len(texts))]
+        texts, embeddings, metadatas=[{"source": f"{i}-pl"} for i in range(len(texts))], persist_directory="./cache"
     )
 
+def load_vecstore():
+    return Chroma(persist_directory="./cache")
 
 def get_chain(transcription_folder: str):
     system_template = """Use the following pieces of context to answer the users question. 
@@ -56,3 +72,7 @@ def get_chain(transcription_folder: str):
         vectorstore=load_vectorstore(transcription_folder),
         chain_type_kwargs=chain_type_kwargs,
     )
+
+
+if __name__  == "__main__":
+    create_vectorstore("./transcriptions")
