b3370288ee93c70724cff90352ea3cb5ccb15299
Rattanakornphan-T <71933012+Rattanakornphan-T@users.noreply.github.com>
2023-04-20 01:42:41 -0500

    Piazza automatic chatbot ver. (almost) 1.0

---
+++ main.py
@@ -0,0 +1,60 @@
+import os
+import warnings # suppress warning for awhile
+from piazza_api import Piazza
+from html2text import HTML2Text
+
+from chain import get_chain
+
+def gpt_reply(course, chain, HTML_2_TEXT, verbose):
+    answer = None
+    fetched_posts = course.iter_all_posts(limit=12) # fetched_posts is a generator
+    list_posts = []
+    for post in fetched_posts:
+        list_posts.append(post)
+        
+    target_post = list_posts[10]
+    if target_post['history'][0]['content'].lower() == 'gpt':
+        html_question = target_post['history'][0]['subject']
+        text_question = HTML_2_TEXT.handle(html_question).strip()
+        if verbose:
+            print("Question:", text_question)
+        answer = chain({"question": text_question}, return_only_outputs=True)
+        if verbose:
+            print("Answer:", answer)
+        course._rpc.content_student_answer(target_post['id'], answer['answer'], 10000, False)
+    return answer
+
+def main():
+    print("Initialize the program...")
+    EMAIL = "rattanakornp@wisc.edu" # Piazza username
+    PASSWORD = "87lemons" # Piazza password
+    network_id = "lcguqjpvo0q39j" # Course CS 538
+    OPENAI_API_KEY = "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"
+    os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+    piazza = Piazza()
+    piazza.user_login(email=EMAIL, password=PASSWORD)
+    course = piazza.network(network_id)
+
+    HTML_2_TEXT = HTML2Text()
+    HTML_2_TEXT.ignore_links = True
+    with warnings.catch_warnings(): # suppress warning for awhile
+        warnings.simplefilter("ignore")
+        chain = get_chain("cache")
+    print("Done initialization!")
+    
+    while True:
+        user_input = input()
+        if len(user_input) == 0:
+            print("Start answering...")
+            gpt_reply(course, chain, HTML_2_TEXT, False)
+            print("Answered!")
+        elif user_input.lower() == "exit":
+            print("Bye!")
+            break
+        else:
+            gpt_reply(course, chain, HTML_2_TEXT, True)
+            print("Answered!")
+
+if __name__ == "__main__":
+    main()
